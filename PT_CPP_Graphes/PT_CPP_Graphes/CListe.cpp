493ad09632cb1766182d6b7babed014d4062bd6c
Mathh267 <mathh267@gmail.com>
2023-05-15 10:11:18 +0200

    Ajout des Cartouches pour CListes.

--- PT_CPP_Graphes/PT_CPP_Graphes/CListe.cpp
+++ PT_CPP_Graphes/PT_CPP_Graphes/CListe.cpp
@@ -3,12 +3,31 @@
 #include "CListe.h"
 
 
-
+/*****************************************************************************
+***** CLISTE<MTYPE> : Constructeur par défaut de CLISTE<MTYPE>	   ***********
+******************************************************************************
+***** Entrée :                                                           *****
+***** Nécessite :                                                        *****
+***** Sortie :                                                           *****
+***** Entraine : Le contructeur a initialisé un objet                    *****
+***** CLISTE<MTYPE> avec uiLISTaille à 0 et le pointeur pLISListe aussi  *****
+***** à 0 et pMATMatrice à NULL                                          *****
+*****************************************************************************/
 template<class MTYPE> CListe<MTYPE>::CListe() {
 	pLISListe = nullptr;
 	uiLISTaille = 0;
 }
 
+/******************************************************************************
+***** CLISTE<MTYPE> : Constructeur de recopie de CLISTE<MTYPE>	   ************
+*******************************************************************************
+***** Entrée :LisParam,un objet de type CListe<MTYPE>                     *****
+***** Nécessite :                                                         *****
+***** Sortie :                                                            *****
+***** Entraine : Le contructeur a initialisé un objet                     *****
+***** CLISTE<MTYPE> qui est une recopie de la liste passé en paramètre OU *****
+***** EXCEPTION ListeVide: On ne recopie pas une liste vide               *****
+******************************************************************************/
 template<class MTYPE> CListe<MTYPE>::CListe(CListe<MTYPE> &LISParam) {
 	if (LISParam.LISLireTaille() == 0) {
 		CException EXCErreur;
@@ -22,6 +41,16 @@ template<class MTYPE> CListe<MTYPE>::CListe(CListe<MTYPE> &LISParam) {
 	}
 }
 
+/******************************************************************************
+***** CLISTE<MTYPE> : Constructeur de confort de CLISTE<MTYPE>	   ************
+*******************************************************************************
+***** Entrée :uiTaille, entier non signé donnant la taille de la liste    *****
+***** Nécessite :                                                         *****
+***** Sortie :                                                            *****
+***** Entraine : Le contructeur a initialisé un objet                     *****
+***** CLISTE<MTYPE> avec la taille passé en paramètre  OU                 *****
+***** EXCEPTION ListeVide: On ne crée pas une liste vide                  *****
+******************************************************************************/
 template<class MTYPE> CListe<MTYPE>::CListe(unsigned int uiTaille) {
 	if (uiTaille == 0) {
 		CException EXCErreur;
@@ -35,10 +64,27 @@ template<class MTYPE> CListe<MTYPE>::CListe(unsigned int uiTaille) {
 	}
 }
 
+/******************************************************************************************
+***** LISLIRETAILLE : Accesseur en lecture de la taille de CLISTE<MTYPE>	   ************
+*******************************************************************************************
+***** Entrée :uiTaille, entier non signé donnant la taille de la liste                *****
+***** Nécessite :                                                                     *****
+***** Sortie : entier non signé, taille de la liste                                   *****
+***** Entraine : LISLireTaille()=uiLISTaille                                          *****
+******************************************************************************************/
 template<class MTYPE> unsigned int CListe<MTYPE>::LISLireTaille() {
 	return uiLISTaille;
 }
 
+/************************************************************************************************
+***** LISLIREELEMENT : Permet d'acceder à l'element en uiPosition de la liste   *****************
+*************************************************************************************************
+***** Entrée :uiPosition, entier non signé de la position de l'element                      *****
+***** Nécessite :                                                                           *****
+***** Sortie : type générique MTYPE, correspondant à l'element de la position de la liste   *****
+***** Entraine : LISLireElement()=pLISListe[uiPosition] OU                                  *****
+***** EXCEPTION DepassementDeTaille: Essaye d'acceder à un element hors de la liste         *****
+************************************************************************************************/
 template<class MTYPE> MTYPE CListe<MTYPE>::LISLireElement(unsigned int uiPosition) {
 	if (uiPosition >= uiLISTaille) {
 		CException EXCErreur;
@@ -48,7 +94,15 @@ template<class MTYPE> MTYPE CListe<MTYPE>::LISLireElement(unsigned int uiPositio
 	return pLISListe[uiPosition];
 }
 
-
+/************************************************************************************************
+***** LISAJOUTERELEMENT : Permet d'ajouter un element en fin de la liste         ****************
+*************************************************************************************************
+***** Entrée :MTYPEParam, type générique, correspondant à l'element                         *****
+***** Nécessite :                                                                           *****
+***** Sortie :																				*****
+***** Entraine : Reallouer la nouvelle liste avec une taille plus grande de 1 et rajouter   *****
+***** l'element à la fin de la liste														*****
+************************************************************************************************/
 template<class MTYPE> void CListe<MTYPE>::LISAjouterElement(MTYPE MTYPEParam) {
 	
 	MTYPE* pNouvelleListe = new MTYPE[uiLISTaille+1];
@@ -60,6 +114,16 @@ template<class MTYPE> void CListe<MTYPE>::LISAjouterElement(MTYPE MTYPEParam) {
 	uiLISTaille = uiLISTaille + 1;
 }
 
+/************************************************************************************************
+***** LISMODIFIERELEMENT : Permet de modifier un element à la uiPosition de la liste ************
+*************************************************************************************************
+***** Entrée :uiPosition, entier non signé correspond à la position de l'element à modifier *****
+***** MTYPEParam, type générique, correspondant à l'element qui va remplacer l'autre        *****
+***** Nécessite :                                                                           *****
+***** Sortie :																				*****
+***** Entraine : pLISListe[uiPosition]=MTYPE OU											    *****
+***** EXCEPTION DepassementDeTaille: Ne peut pas modifier un element hors de la liste		*****
+************************************************************************************************/
 template<class MTYPE> void CListe<MTYPE>::LISModifierElement(unsigned int uiPosition, MTYPE MTYPEParam) {
 	if (uiPosition >= uiLISTaille) {
 		CException EXCErreur;
@@ -69,6 +133,16 @@ template<class MTYPE> void CListe<MTYPE>::LISModifierElement(unsigned int uiPosi
 	pLISListe[uiPosition] = MTYPEParam;
 }
 
+/*************************************************************************************************
+***** LISSUPPRIMERELEMENT : Permet de supprimer un element à la uiPosition de la liste ***********
+**************************************************************************************************
+***** Entrée :uiPosition, entier non signé correspond à la position de l'element à supprimer *****
+***** Nécessite :                                                                            *****
+***** Sortie :																				 *****
+***** Entraine : La nouvelle liste avec tous les elements de la liste avant uiPosition non   *****
+***** modifié et autres avec un decalage de 1 OU                                             *****
+***** EXCEPTION DepassementDeTaille: Ne peut pas supprimer un element hors de la liste		 *****
+*************************************************************************************************/
 template<class MTYPE> void CListe<MTYPE>::LISSupprimerElement(unsigned int uiPosition) {
 	if (uiPosition >= uiLISTaille) {
 		CException EXCErreur;
@@ -88,6 +162,15 @@ template<class MTYPE> void CListe<MTYPE>::LISSupprimerElement(unsigned int uiPos
 	uiLISTaille = uiLISTaille - 1;
 }
 
+/*************************************************************************************************
+***** LISAFFICHER : Permet d'afficher la liste                                        ************
+**************************************************************************************************
+***** Entrée :																			     *****
+***** Nécessite :                                                                            *****
+***** Sortie :																				 *****
+***** Entraine : Affiche dans la console la liste                                            *****
+***** EXCEPTION ListeVide: On n'affiche pas une liste vide		                             *****
+*************************************************************************************************/
 template<class MTYPE> void CListe<MTYPE>::LISAfficher() {
 	if (uiLISTaille == 0) {
 		CException EXCErreur;
@@ -100,6 +183,15 @@ template<class MTYPE> void CListe<MTYPE>::LISAfficher() {
 	cout << "\n";
 }
 
+/*************************************************************************************************
+***** OPERATOR= : Surcharge de l'opérateur =                                          ************
+**************************************************************************************************
+***** Entrée :	LISParam , CListe<MTYPE> à recopier										     *****
+***** Nécessite :                                                                            *****
+***** Sortie : CListe<MTYPE>, recopie de LISParam											 *****
+***** Entraine : Recopie la liste LISParam et la renvoie                                     *****
+***** EXCEPTION ListeVide: On n'affiche pas une liste vide		                             *****
+*************************************************************************************************/
 template<class MTYPE> CListe<MTYPE>& CListe<MTYPE>::operator=(CListe<MTYPE> LISParam) {
 	
 	unsigned int uiTaille = LISParam.LISLireTaille();
