a2fa8ba34ed7b86d877ed04420608cd2a9e120fc
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2023-05-19 19:40:28 +0200

    Rédaction des cartouches de commentaires pour CArc

--- PT_CPP_Graphes/PT_CPP_Graphes/CArc.cpp
+++ PT_CPP_Graphes/PT_CPP_Graphes/CArc.cpp
@@ -1,21 +1,60 @@
 #include "CArc.h"
 
+/*************************************************
+***** CARC : Constructeur par défaut de CArc *****
+**************************************************
+***** Entrée :                               *****
+***** Nécessite :                            *****
+***** Sortie :                               *****
+***** Entraine : Le contructeur a initialisé *****
+***** un objet CLecteur avec                 *****
+***** uiARCDestination égal à NULL           *****
+*************************************************/
 CArc::CArc()
 {
 	uiARCDestination = NULL;
 }
 
+/*************************************************
+***** CARC : Constructeur de confort de CArc *****
+**************************************************
+***** Entrée : uiDestination, entier non     *****
+***** signé, identifiant du sommet de        *****
+***** destination                            *****
+***** Nécessite :                            *****
+***** Sortie :                               *****
+***** Entraine : Le contructeur a initialisé *****
+***** un objet CArc ayant                    *****
+***** uiARCDestination = uiDestination       *****
+*************************************************/
 CArc::CArc(unsigned int uiDestination)
 {
 	uiARCDestination = uiDestination;
 }
 
+/*******************************************************************************
+***** ARCLIREDESTINATION : Accesseur direct en lecture de uiARCDestination *****
+********************************************************************************
+***** Entrée :                                                             *****
+***** Nécessite :                                                          *****
+***** Sortie : entier non signé, valeur de l'attribut uiARCDestination     *****
+***** Entraine : ARCLireDestination() = uiARCDestination                   *****
+*******************************************************************************/
 unsigned int CArc::ARCLireDestination()
 {
 	return uiARCDestination;
 }
 
-void CArc::ARCModifierDestination(int uiDestination)
+/************************************************************************************
+***** ARCMODIFIERDESTINATION : Accesseur direct en écriture de uiARCDestination *****
+*************************************************************************************
+***** Entrée : uiDestination, entier non signé, nouvelle valeur de              *****
+***** uiARCDestination                                                          *****
+***** Nécessite :                                                               *****
+***** Sortie :                                                                  *****
+***** Entraine : uiARCDestination = uiDestination                               *****
+************************************************************************************/
+void CArc::ARCModifierDestination(unsigned int uiDestination)
 {
 	uiARCDestination = uiDestination;
 }
--- PT_CPP_Graphes/PT_CPP_Graphes/CArc.h
+++ PT_CPP_Graphes/PT_CPP_Graphes/CArc.h
@@ -6,17 +6,55 @@
 class CArc
 {
 	private:
-		unsigned int uiARCDestination;
+		unsigned int uiARCDestination; //Entier non signé, Identifiant du sommet de destination de l'arc
 
 	public:
-
+		/*************************************************
+		***** CARC : Constructeur par défaut de CArc *****
+		**************************************************
+		***** Entrée :                               *****
+		***** Nécessite :                            *****
+		***** Sortie :                               *****
+		***** Entraine : Le contructeur a initialisé *****
+		***** un objet CLecteur avec                 *****
+		***** uiARCDestination égal à NULL           *****
+		*************************************************/
 		CArc();
 
+		/*************************************************
+		***** CARC : Constructeur de confort de CArc *****
+		**************************************************
+		***** Entrée : uiDestination, entier non     *****
+		***** signé, identifiant du sommet de        *****
+		***** destination                            *****
+		***** Nécessite :                            *****
+		***** Sortie :                               *****
+		***** Entraine : Le contructeur a initialisé *****
+		***** un objet CArc ayant                    *****
+		***** uiARCDestination = uiDestination       *****
+		*************************************************/
 		CArc(unsigned int uiDestination);
 
+		/*******************************************************************************
+		***** ARCLIREDESTINATION : Accesseur direct en lecture de uiARCDestination *****
+		********************************************************************************
+		***** Entrée :                                                             *****
+		***** Nécessite :                                                          *****
+		***** Sortie : entier non signé, valeur de l'attribut uiARCDestination     *****
+		***** Entraine : ARCLireDestination() = uiARCDestination                   *****
+		*******************************************************************************/
 		unsigned int ARCLireDestination();
 
-		void ARCModifierDestination(int uiDestination);
+		/************************************************************************************
+		***** ARCMODIFIERDESTINATION : Accesseur direct en écriture de uiARCDestination *****
+		*************************************************************************************
+		***** Entrée : uiDestination, entier non signé, nouvelle valeur de              *****
+		***** uiARCDestination                                                          *****
+		***** Nécessite :                                                               *****
+		***** Sortie :                                                                  *****
+		***** Entraine : uiARCDestination = uiDestination                               *****
+		************************************************************************************/
+		void ARCModifierDestination(unsigned int uiDestination);
 
 };
 
