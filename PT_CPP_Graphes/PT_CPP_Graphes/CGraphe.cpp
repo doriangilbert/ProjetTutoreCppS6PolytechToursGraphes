27a23b33acb548c7fd4d91184c3fbef4f658a866
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2023-05-19 21:24:21 +0200

    Rédaction des cartouches de commentaires et de fonctions élémentaires pour CGraphe

--- PT_CPP_Graphes/PT_CPP_Graphes/CGraphe.cpp
+++ PT_CPP_Graphes/PT_CPP_Graphes/CGraphe.cpp
@@ -1,47 +1,117 @@
 #include "CGraphe.h"
 
+/*******************************************************
+***** CGRAPHE : Constructeur par défaut de CGraphe *****
+********************************************************
+***** Entrée :                                     *****
+***** Nécessite :                                  *****
+***** Sortie :                                     *****
+***** Entraine : Le contructeur a initialisé un    *****
+***** objet CGraphe avec bGRAEstOriente            *****
+***** égal à true                                  *****
+*******************************************************/
 CGraphe::CGraphe()
 {
-
+	bGRAEstOriente = true;
 }
 
-CGraphe::CGraphe(CGraphe &GRAParam)
+/*CGraphe::CGraphe(CGraphe& GRAParam)
 {
 
-}
+}*/
 
+/*******************************************************
+***** CGRAPHE : Constructeur de confort de CGraphe *****
+********************************************************
+***** Entrée : bEstOriente, booléen, indication du *****
+***** type de graphe (orienté ou non)              *****
+***** Nécessite :                                  *****
+***** Sortie :                                     *****
+***** Entraine : Le contructeur a initialisé       *****
+***** un objet CGraphe ayant                       *****
+***** bGRAEstOriente = bEstOriente                 *****
+*******************************************************/
 CGraphe::CGraphe(bool bEstOriente)
 {
-	
+	bGRAEstOriente = bEstOriente;
 }
 
-CGraphe::~CGraphe()
+/*CGraphe::~CGraphe()
 {
 
-}
+}*/
 
+/****************************************************************************
+***** GRALIREESTORIENTE : Accesseur direct en lecture de bGRAEstOriente *****
+*****************************************************************************
+***** Entrée :                                                          *****
+***** Nécessite :                                                       *****
+***** Sortie : booléen, valeur de l'attribut bGRAEstOriente             *****
+***** Entraine : GRALireEstOriente() = bGRAEstOriente                   *****
+****************************************************************************/
 bool CGraphe::GRALireEstOriente()
 {
 	return bGRAEstOriente;
 }
 
+/***************************************************************************
+***** GRALIRELISTESOMMET : Accesseur direct en lecture de LISGRASommet *****
+****************************************************************************
+***** Entrée :                                                         *****
+***** Nécessite :                                                      *****
+***** Sortie : Objet de type CListe<CSommet>, valeur de l'attribut     *****
+***** LISGRASommet                                                     *****
+***** Entraine : GRALireListeSommet() = LISGRASommet                   *****
+***************************************************************************/
+CListe<CSommet> CGraphe::GRALireListeSommet()
+{
+	return LISGRASommet;
+}
+
+/*******************************************************************************
+***** GRAAJOUTERSOMMET : Fonction permettant d'ajouter un sommet au graphe *****
+********************************************************************************
+***** Entrée : uiId, entier non signé, identifiant du sommet à ajouter     *****
+***** Nécessite :                                                          *****
+***** Sortie :                                                             *****
+***** Entraine : LISGRASommet contient la liste des sommets du graphe,     *****
+***** additionnée du sommet à ajouter                                      *****
+*******************************************************************************/
 void CGraphe::GRAAjouterSommet(unsigned int uiId)
 {
 
 }
 
+/************************************************************************************
+***** GRASUPPRIMERSOMMET : Fonction permettant de supprimer un sommet du graphe *****
+*************************************************************************************
+***** Entrée : uiId, entier non signé, identifiant du sommet à supprimer        *****
+***** Nécessite :                                                               *****
+***** Sortie :                                                                  *****
+***** Entraine : LISGRASommet contient la liste des sommets du graphe,          *****
+***** soustraite du sommet à supprimer                                          *****
+************************************************************************************/
 void CGraphe::GRASupprimerSommet(unsigned int uiId)
 {
 
 }
 
+/***************************************************************************
+***** GRAAFFICHER : Fonction permettant d'afficher un graphe à l'écran *****
+****************************************************************************
+***** Entrée :                                                         *****
+***** Nécessite :                                                      *****
+***** Sortie :                                                         *****
+***** Entraine : La fonction à affiché les informations du graphe à    *****
+***** l'écran                                                          *****
+***************************************************************************/
 void CGraphe::GRAAfficher()
 {
 
 }
 
-CGraphe& CGraphe::operator=(CGraphe &GRAParam)
+/*CGraphe& CGraphe::operator=(CGraphe& GRAParam)
 {
 	CGraphe TODO;
 	return TODO;
-}
+}*/
--- PT_CPP_Graphes/PT_CPP_Graphes/CGraphe.h
+++ PT_CPP_Graphes/PT_CPP_Graphes/CGraphe.h
@@ -9,28 +9,94 @@
 class CGraphe
 {
 	private:
-		CListe<CSommet> LISGRASommet;
-		bool bGRAEstOriente;
+		CListe<CSommet> LISGRASommet; //Liste des sommets du graphe
+		bool bGRAEstOriente; //Booléen, Indique si le graphe est orienté ou non (true si orienté et false si non orienté)
 
 	public:
-
+		/*******************************************************
+		***** CGRAPHE : Constructeur par défaut de CGraphe *****
+		********************************************************
+		***** Entrée :                                     *****
+		***** Nécessite :                                  *****
+		***** Sortie :                                     *****
+		***** Entraine : Le contructeur a initialisé un    *****
+		***** objet CGraphe avec bGRAEstOriente            *****
+		***** égal à true                                  *****
+		*******************************************************/
 		CGraphe();
 
-		CGraphe(CGraphe &GRAParam);
+		//CGraphe(CGraphe &GRAParam);
 
+		/*******************************************************
+		***** CGRAPHE : Constructeur de confort de CGraphe *****
+		********************************************************
+		***** Entrée : bEstOriente, booléen, indication du *****
+		***** type de graphe (orienté ou non)              *****
+		***** Nécessite :                                  *****
+		***** Sortie :                                     *****
+		***** Entraine : Le contructeur a initialisé       *****
+		***** un objet CGraphe ayant                       *****
+		***** bGRAEstOriente = bEstOriente                 *****
+		*******************************************************/
 		CGraphe(bool bEstOriente);
 
-		~CGraphe();
+		//~CGraphe();
 
+		/****************************************************************************
+		***** GRALIREESTORIENTE : Accesseur direct en lecture de bGRAEstOriente *****
+		*****************************************************************************
+		***** Entrée :                                                          *****
+		***** Nécessite :                                                       *****
+		***** Sortie : booléen, valeur de l'attribut bGRAEstOriente             *****
+		***** Entraine : GRALireEstOriente() = bGRAEstOriente                   *****
+		****************************************************************************/
 		bool GRALireEstOriente();
 
+		/***************************************************************************
+		***** GRALIRELISTESOMMET : Accesseur direct en lecture de LISGRASommet *****
+		****************************************************************************
+		***** Entrée :                                                         *****
+		***** Nécessite :                                                      *****
+		***** Sortie : Objet de type CListe<CSommet>, valeur de l'attribut     *****
+		***** LISGRASommet                                                     *****
+		***** Entraine : GRALireListeSommet() = LISGRASommet                   *****
+		***************************************************************************/
+		CListe<CSommet> GRALireListeSommet();
+
+		/*******************************************************************************
+		***** GRAAJOUTERSOMMET : Fonction permettant d'ajouter un sommet au graphe *****
+		********************************************************************************
+		***** Entrée : uiId, entier non signé, identifiant du sommet à ajouter     *****
+		***** Nécessite :                                                          *****
+		***** Sortie :                                                             *****
+		***** Entraine : LISGRASommet contient la liste des sommets du graphe,     *****
+		***** additionnée du sommet à ajouter                                      *****
+		*******************************************************************************/
 		void GRAAjouterSommet(unsigned int uiId);
 
+		/************************************************************************************
+		***** GRASUPPRIMERSOMMET : Fonction permettant de supprimer un sommet du graphe *****
+		*************************************************************************************
+		***** Entrée : uiId, entier non signé, identifiant du sommet à supprimer        *****
+		***** Nécessite :                                                               *****
+		***** Sortie :                                                                  *****
+		***** Entraine : LISGRASommet contient la liste des sommets du graphe,          *****
+		***** soustraite du sommet à supprimer                                          *****
+		************************************************************************************/
 		void GRASupprimerSommet(unsigned int uiId);
 
+		/***************************************************************************
+		***** GRAAFFICHER : Fonction permettant d'afficher un graphe à l'écran *****
+		****************************************************************************
+		***** Entrée :                                                         *****
+		***** Nécessite :                                                      *****
+		***** Sortie :                                                         *****
+		***** Entraine : La fonction à affiché les informations du graphe à    *****
+		***** l'écran                                                          *****
+		***************************************************************************/
 		void GRAAfficher();
 
-		CGraphe& operator=(CGraphe &GRAParam);
+		//CGraphe& operator=(CGraphe &GRAParam);
 
 };
 
